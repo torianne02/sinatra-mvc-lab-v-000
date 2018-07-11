75fc57c27ebc955c98fe1a345e790ac581370b63
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-11 23:04:28 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,6 +6,24 @@ class PigLatinizer
   end
 
   def piglatinize(phrase)
-    @phrase
+    @phrase = phrase.split(' ')
 
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u]
+    consonants = alpha - vowels
+    @answer = []
+
+    @text.each do |word|
+      if vowels.include?(word[0].downcase)
+        @answer << word + 'way'
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1]) && consonants.include?(word[2])
+        @answer << word[3..-1] + word[0..1] + word[2] + 'ay'
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1])
+        @answer << word[2..-1] + word[0..1] + 'ay'
+      else consonants.include?(word[0].downcase)
+        @answer << word[1..-1] + word[0] + 'ay'
+      end
+    end
+    @answer.join(',').gsub(/[\s,]/ ," ")
+  end
 end
